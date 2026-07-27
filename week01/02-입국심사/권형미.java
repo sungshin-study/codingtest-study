0d2ae74891626231b2a7feb1132b54e9c79f9b6c
hyomee2 <s5763305@naver.com>
2026-07-27 22:46:44 +0900

    [형미] week01/입국심사 풀이 (수정)

--- week01/02-입국심사/권형미.java
+++ week01/02-입국심사/권형미.java
@@ -1,54 +1,25 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int[][] costs) {
-        int answer = 0;
-        int count = 0;
-        boolean[] visited = new boolean[n];
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long left = 1;
+        long right = (long)times[0] * n;
+        long answer = right;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-                (a, b) -> a[1] - b[1]
-        );
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long sum = 0;
 
-        ArrayList<int[]>[] graph = new ArrayList[n];
-        for (int i = 0; i < graph.length; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < costs.length; i++) {
-            int start = costs[i][0];
-            int end = costs[i][1];
-            int cost = costs[i][2];
-
-            graph[start].add(new int[]{end, cost});
-            graph[end].add(new int[]{start, cost});
-        }
-
-        for (int[] node : graph[0]) {
-            pq.offer(node);
-            visited[0] = true;
-            count = 1;
-        }
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-
-            if (visited[cur[0]]) {
-                continue;
+            for (int i = 0; i < times.length; i++) {
+                sum += (mid / times[i]);
             }
 
-            visited[cur[0]] = true;
-            answer += cur[1];
-            count++;
-
-            if (count == n) {
-                break;
-            }
-
-            for (int[] node : graph[cur[0]]) {
-                if (!visited[node[0]]) {
-                    pq.offer(node);
-                }
+            if (sum >= n) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
 
